716bdbf3906579ae7d701aa7207a7aa7dc14c98e
Prakash Kumar Singh <prakashaug98@gmail.com>
2024-09-14 18:55:24 +0530

    feat: handle sum of two numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,10 @@
 function add(numbers) {
   if (numbers === "") return 0;
   if (!isNaN(numbers)) return parseInt(numbers, 10);
-  return null;
+  return numbers
+    .split(/,|\n/)
+    .map(Number)
+    .reduce((a, b) => a + b, 0);
 }
 
 module.exports = { add };
