875b476bcbdbe31444cd789cc39b5b9353574a8a
Prakash Kumar Singh <prakashaug98@gmail.com>
2024-09-15 17:34:52 +0530

    feat: support custom delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,4 +1,12 @@
 function add(numbers) {
+  if (numbers.startsWith("//")) {
+    const delimiter = numbers[2];
+    numbers = numbers.slice(4);
+    return numbers
+      .split(delimiter)
+      .map(Number)
+      .reduce((a, b) => a + b, 0);
+  }
   if (numbers === "") return 0;
   if (!isNaN(numbers)) return parseInt(numbers, 10);
   return numbers
